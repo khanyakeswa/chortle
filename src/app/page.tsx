d8286103dec121e93fccf41f7c605ac3f6a428eb
Sandile Keswa <me@sandile.io>
2023-05-28 22:02:51 -0400

    just merged fam

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,27 @@
-import Head from 'next/head'
-import Link from 'next/link'
-import type { MouseEventHandler } from 'react'
+import Head from "next/head";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-
   return (
-    <main className='flex min-h-screen bg-slate-100 flex-col items-center justify-between p-12'>
+    <main className="flex min-h-screen bg-slate-100 flex-col items-center justify-between p-12">
       <Head>
-        <link rel='icon' type='image/svg+xml' href='/favicon/favicon.svg' />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
       </Head>
-      <div className='flex z-10 w-full max-w-5xl text-gray-900 items-center justify-between font-mono text-sm lg:flex'>
+      <div className="flex z-10 w-full max-w-5xl text-gray-900 items-center justify-between font-mono text-sm lg:flex">
         <h1>Welcome to Chortle: Chores Made Simple</h1>
+        <UserButton afterSignOutUrl="/" />
         <div>
-          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-4 rounded'>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-4 rounded">
             Login
           </button>
-          <Link href='/login'>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-4 rounded'>
+          <Link href="/login">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-4 rounded">
               Sign-up
             </button>
           </Link>
         </div>
       </div>
     </main>
-  )
+  );
 }
